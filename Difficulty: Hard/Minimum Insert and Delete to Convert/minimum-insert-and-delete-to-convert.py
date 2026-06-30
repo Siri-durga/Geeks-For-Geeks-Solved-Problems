348c595a15e59f1a1c2b7aac9d4fad4102d9f3e1
DURGA LALITHA SRI VARSHITHA <23A91A0516@aec.edu.in>
2026-06-30 19:51:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum Insert and Delete to Convert/minimum-insert-and-delete-to-convert.py
@@ -0,0 +1,23 @@
+from bisect import bisect_left
+
+class Solution:
+    def minInsAndDel(self, a, b):
+        pos = {val: i for i, val in enumerate(b)}
+
+        seq = []
+        for x in a:
+            if x in pos:
+                seq.append(pos[x])
+
+        lis = []
+
+        for x in seq:
+            idx = bisect_left(lis, x)
+            if idx == len(lis):
+                lis.append(x)
+            else:
+                lis[idx] = x
+
+        lcs = len(lis)
+
+        return (len(a) - lcs) + (len(b) - lcs)
